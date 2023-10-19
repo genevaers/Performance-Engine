8d9643b0ea0c747c7df9a3f26e4216ef67c4b566
Neil Beesley <neil.beesley@ibm.com>
2023-10-19 13:34:20 -0500

    DB2HPU

--- ASM/GVBMRSU.asm
+++ ASM/GVBMRSU.asm
@@ -1,27 +1,25 @@
-         TITLE 'GVBMRSU - DB2 HPU INITIALIZATION FOR "GVBMR96"'
-********************************************************************#VB
-*   SAFR                                                              *
-********************************************************************#VE
+         TITLE 'GVBMRSU - DB2 HPU INITIALIZATION FOR GVBMR96'
+**********************************************************************
 *
-* (c) Copyright IBM Corporation 2008.
+* (C) COPYRIGHT IBM CORPORATION 2022, 2023.
 *     Copyright Contributors to the GenevaERS Project.
 * SPDX-License-Identifier: Apache-2.0
 *
-***********************************************************************
+**********************************************************************
 *
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
 *
 *     http://www.apache.org/licenses/LICENSE-2.0
 *
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-*   or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-***********************************************************************
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+*  or implied.
+*  See the License for the specific language governing permissions
+*  and limitations under the License.
+*
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                                                                     *
 *   GVBMRSU -                                                         *
